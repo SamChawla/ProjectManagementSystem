2d694a8e46f29e92260441abdfce5407b4acd1cb
Sumit Chawla <sam.chawla26@gmail.com>
2024-10-10 22:00:29 +0530

    WIP: User registration and login

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,6 @@
 """ This module defines the forms used in the accounts app."""
 
+from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
@@ -47,3 +48,49 @@ class CustomUserChangeForm(UserChangeForm):
             "groups",
             "user_permissions",
         ]
+
+
+class UserRegistrationForm(UserCreationForm):
+    """This class defines the form for creating a new user"""
+
+    class Meta(UserCreationForm.Meta):
+        """This class defines the metadata for the CustomUserCreationForm"""
+
+        model = User
+        fields = [
+            "email",
+            "first_name",
+            "last_name",
+            "password1",
+            "password2",
+        ]
+
+    def __init__(self, *args, **kwargs):
+        super(UserRegistrationForm, self).__init__(*args, **kwargs)
+        for _, field in self.fields.items():
+            existing_classes = field.widget.attrs.get("class", "")
+            field.widget.attrs["class"] = f"form-control {existing_classes}"
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.is_active = False
+        if commit:
+            user.save()
+        return user
+
+
+class UserLoginForm(forms.Form):
+    """This class defines the form for logging in a user"""
+
+    email = forms.EmailField(
+        widget=forms.EmailInput(
+            attrs={"class": "form-control", "placeholder": "Email"}
+        ),
+        label="Email",
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={"class": "form-control", "placeholder": "Password"}
+        ),
+        label="Password",
+    )
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,93 @@
-from django.shortcuts import render
+""" This file is used to create views for the accounts app. """
 
-# Create your views here.
+from django.shortcuts import redirect, render
+from django.contrib import messages
+from django.views import View
+from django.contrib.auth import authenticate, logout
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .forms import UserLoginForm, UserRegistrationForm
+
+
+# def register(request):
+#     if request.method == "POST":
+#         form = UserRegistrationForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return redirect("login")
+#     else:
+#         form = UserRegistrationForm()
+#     context = {
+#         "form": form,
+#         "title": "Register User",
+#     }
+#     return render(request, "accounts/register.html", context=context)
+
+class RegisterView(View):
+    """This class-based view handles user registration."""
+
+    def get(self, request):
+        """Handles GET requests to the register view."""
+        form = UserRegistrationForm()
+        context = {
+            "form": form,
+            "title": "Register User",
+        }
+        return render(request, "accounts/register.html", context=context)
+
+    def post(self, request):
+        """Handles POST requests to the register view."""
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Account created successfully. Please Contact admin for account activation.")
+            return redirect("login")
+        context = {
+            "form": form,
+            "title": "Register User",
+        }
+        return render(request, "accounts/register.html", context=context)
+
+
+class LoginView(View):
+    """This class-based view handles user login."""
+
+    template_name = "accounts/login.html"
+
+    def get(self, request):
+        """Handles GET requests to the login view."""
+        form = UserLoginForm()
+        context = {
+            "form": form,
+            "title": "Login",
+        }
+        return render(request, self.template_name, context=context)
+
+    def post(self, request):
+        """Handles POST requests to the login view."""
+        form = UserLoginForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data.get("email")
+            password = form.cleaned_data.get("password")
+            user = authenticate(email=email, password=password)
+            if user is None:
+                messages.error(request, "Invalid email or password.")
+                return redirect("login")
+            if not user.is_active:
+                messages.error(request, "Account is not active. Please contact admin.")
+                return redirect("login")
+            messages.success(request, "Login successful.")
+            return redirect("home")
+        context = {
+            "form": form,
+            "title": "Login",
+        }
+        return render(request, "accounts/login.html", context=context)
+
+class LogoutView(LoginRequiredMixin, View):
+    """ This class-based view handles user logout. """
+
+    def get(self, request):
+        """ Handles GET requests to the logout view. """
+        logout(request)
+        messages.success(request, "You are logged out successfully.")
+        return redirect("login")
