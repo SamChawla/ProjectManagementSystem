320d35ac6c67a115f932959627f848c668ef0246
Sumit Chawla <sam.chawla26@gmail.com>
2024-10-12 13:34:25 +0530

    WIP: user registration and login
    - Created Forms for User Registration and Login
    - Added Base Templates
    - Added Static Files

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+""" This file contains the views for the accounts app."""
 
-# Create your views here.
+from django.shortcuts import redirect, render
+from django.contrib import messages
+from django.contrib.auth import get_user_model
+from django.views import View
+from .forms import UserRegistrationForm, UserLoginForm
+
+User = get_user_model()
+
+
+# Function Based View
+# def register(request):
+#     """This function renders the registration form."""
+#     if request.method == "POST":
+#         form = CustomUserCreationForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return redirect("login")
+#     else:
+#         form = CustomUserCreationForm()
+#     return render(request, "registration/register.html", {"form": form})
+
+
+# Class Based View
+
+class RegisterView(View):
+    """This class renders the registration form."""
+
+    def get(self, request):
+        """This method handles the GET request."""
+        form = UserRegistrationForm()
+        return render(request, "accounts/register.html", {"form": form})
+
+    def post(self, request):
+        """This method handles the POST request."""
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "User created successfully. Please ask admin to activate your account.")
+            return redirect("login")
+        return render(request, "accounts/register.html", {"form": form})
